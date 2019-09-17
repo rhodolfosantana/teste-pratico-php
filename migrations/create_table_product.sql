78cea85f0c0ba89f9588ce2b8b38bde84d4b22f5
rhodolfosantana <rhodolfo.dodo@hotmail.com>
2019-09-17 11:13:15 -0300

    Ajustando nome das colunas ao cenário.

---
+++ migrations/create_table_product.sql
@@ -0,0 +1,7 @@
+create table product(
+    name_porduct varchar(50) NOT NULL,
+    date_sale date NOT NULL,
+    address int(10) NOT NULL,
+
+    PRIMARY KEY (name_porduct)
+);
--- migrations/create_table_sales.sql
+++ migrations/create_table_sales.sql
@@ -1,10 +1,10 @@
-create table users(
-    id_user int NOT NULL AUTO_INCREMENT,
-    name varchar(100) NOT NULL,
-    age int NOT NULL,
-	colors_id int(11) NOT NULL,
+create table sales(
+    id_sales int NOT NULL AUTO_INCREMENT,
+    sales_product varchar(100) NOT NULL,
+    value int NOT NULL,
+    provider varchar(100) NOT NULL,
 
-    PRIMARY KEY (id_user),
-	CONSTRAINT  FOREIGN KEY (colors_id) REFERENCES colors (id_color)
+    PRIMARY KEY (id_sales),
+	CONSTRAINT  FOREIGN KEY (sales_product) REFERENCES product(name_product)
 
 );
