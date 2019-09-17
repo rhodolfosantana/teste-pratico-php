26fde1d9aaca21e6807994171306e096653aa516
rhodolfosantana <rhodolfo.dodo@hotmail.com>
2019-09-17 11:25:51 -0300

    finalizando parte de banco de dados

--- migrations/create_table_product.sql
+++ migrations/create_table_product.sql
@@ -1,7 +1,8 @@
 create table product(
+	id_product int NOT NULL AUTO_INCREMENT,
     name_porduct varchar(50) NOT NULL,
     date_sale date NOT NULL,
     address int(10) NOT NULL,
 
-    PRIMARY KEY (name_porduct)
+    PRIMARY KEY (id_product)
 );
--- migrations/create_table_sales.sql
+++ migrations/create_table_sales.sql
@@ -1,10 +1,10 @@
 create table sales(
     id_sales int NOT NULL AUTO_INCREMENT,
-    sales_product varchar(100) NOT NULL,
+    sales_product int NOT NULL,
     value int NOT NULL,
     provider varchar(100) NOT NULL,
 
     PRIMARY KEY (id_sales),
-	CONSTRAINT  FOREIGN KEY (sales_product) REFERENCES product(name_product)
+	CONSTRAINT  FOREIGN KEY (sales_product) REFERENCES product(id_product)
 
 );
